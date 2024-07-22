e7532142f3de1f1bf2cbebd7b8efd7d34a6f7a4d
LizethVictoria20 <lizvictoria754@gmail.com>
2024-07-22 11:05:10 -0500

    Iniciadno proyecto con tailwind

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <>
+      <div className="flex">
+        <div className="flex-none w-14 ...">01</div>
+        <div className="flex-1 w-64 ...">02</div>
+        <div className="flex-1 w-32 ...">03</div>
+      </div>
+    </>
+  );
+};
+export default Header;
