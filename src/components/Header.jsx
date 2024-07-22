28bbb570ec9b67009a24bb4f50ee89e9f875e63a
LizethVictoria20 <lizvictoria754@gmail.com>
2024-07-22 12:56:01 -0500

    Add react router

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import '../styles/Header.css'
 import Logo from '../assets/logo.png';
 import GitHub from '../assets/icon-github.svg';
 import Linkedin from '../assets/icon-linkedin.svg';
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -9,17 +10,17 @@ const Header = () => {
       <header className="absolute inset-x-0 top-0 z-50">
         <nav className="flex items-center justify-between p-6 lg:px-8 max-sm:flex-col" aria-label="Global">
           <div className="flex lg:flex-1 personal-logo">
-            <a href="#" className="-m-1.5 p-1.5">
+            <Link to="/" className="-m-1.5 p-1.5">
               <img className="h-8" src={Logo} alt="Personal logo" />
-            </a>
+            </Link>
           </div>
           <div className="max-sm:flex max-sm:flex-1 max-sm:justify-end max-md:flex max-md:flex-1 max-md:justify-end lg:flex lg:flex-1 lg:justify-end logo-network">
-            <a href="#" className="text-sm font-semibold leading-6">
+            <Link to="https://github.com/LizethVictoria20" className="text-sm font-semibold leading-6" target="_blanc">
               <img src={GitHub} alt="Logo Github" />
-            </a>
-            <a href="#" className="text-sm font-semibold leading-6">
+            </Link>
+            <Link to="https://www.linkedin.com/in/lizethvictoria/" className="text-sm font-semibold leading-6">
               <img src={Linkedin} alt="Logo Linkendin" />
-            </a>
+            </Link>
           </div>
         </nav>
       </header>
